832dcbbd5d381e738e49c6aede6a6e35f39456ff
erickHub03 <utp0153663@alumno.utpuebla.edu.mx>
2023-05-30 20:24:58 -0600

    Actualizacion de texto

--- alerta.js
+++ alerta.js
@@ -3,3 +3,26 @@ const miBoton = document.getElementById('miboton');
 function handleClick(){
     alert('¡Hola! Has hecho click en el Botón');
 }
+
+const form = document.getElementById('myForm');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  validateForm();
+});
+
+function validateEmail(email) {
+    const regex =  /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
+    return regex.test(email)
+  }
+  
+  function validateForm() {
+    const emailInput = document.getElementById('email');
+    const email = emailInput.value;
+  
+    if (!validateEmail(email)) {
+      alert('Por favor ingrese un correo electrónico válido.');
+    } else {
+      alert('Correo electrónico enviado correctamente.');
+    }
+  }
